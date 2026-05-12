48cf1cad7d58d32bd879e6c8cc0b558a544ec3e7
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-05-12 16:30:52 +0530

    fixed auth page

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -4,9 +4,10 @@ import toast from "react-hot-toast"
 import { useNavigate } from "react-router-dom"
 import { checkUser } from "../utils/auth.js"
 import api from "../api/api.js"
+import img from "../assets/pexels-nepal-visuals-2154640351-33330007.jpg"
 
 export default function AuthPage() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm()
   const [mode, setMode] = useState("login")
   const navigate = useNavigate()
   const [user, setUser] = useState(checkUser())
@@ -40,54 +41,87 @@ export default function AuthPage() {
     }
   }
   return (
-    <div className="grid justify-center content-center w-full h-screen">
-      <div className="auth-container flex flex-col text-center gap-2">
-        {mode === 'login' ? (
-          <h1>Login</h1>
-        ) : (
-          <h1>SignUp</h1>
-        )}
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-
-          className="new-form align-text-top sm:gap-2">
-          {mode === 'signup' && (
-            <input
-              className="input-field max-sm:mb-2"
-              type="text"
-              placeholder="Your Name"
-              {...register('name', { required: 'Name is Required' })} />
-          )}
-          <input
-            className="input-field max-sm:mb-2"
-            type="email"
-            placeholder="name@domain.com"
-            {...register('email', { required: 'Email is Required' })} />
-          <input
-            className="input-field max-sm:mb-2"
-            type="password"
-            placeholder="password"
-            {...register('password', { required: 'Password is required' })} />
-          <button className="bg-blue-500 h-8 max-sm:mb-2 rounded-xl hover:cursor-pointer hover:bg-blue-600 text-white" type="submit">
+    <div className="h-screen mx-20 border-l border-r border-zinc-700">
+      <h1 className="absolute top-6 left-28 text-2xl font-extrabold">Trek wiki</h1>
+      <div className="grid grid-cols-2  h-screen bg-zinc-800 ">
+        <img className="p-4 w-full h-full object-cover overflow-hidden object-center bg-cover" src={img} alt="" />
+        <div className="px-28 flex flex-col justify-center h-screen">
+          <div className="mb-8">
+            <h1 className="text-5xl leading-14 mb-2">
+              {mode === "login" ? (
+                'Login to your account '
+              ) : (
+                'Create an account '
+              )}
+            </h1>
             {mode === 'login' ? (
-              'Login'
+              <p className="text-zinc-300">Don't have an account ? <a className="text-blue-500 hover:underline hover:cursor-pointer"
+                onClick={() => setMode("signup")}
+              >Sign Up
+              </a> </p>
             ) : (
-              'SignUp'
+              <p className="text-zinc-300">Already have an account ? <a className="text-blue-500 hover:underline hover:cursor-pointer"
+                onClick={() => setMode("login")}
+              >Login
+              </a> </p>
             )}
-          </button>
-          {mode === 'login' ? (
-            <p className="text-xs text-white">Don't have an account? <a onClick={() => setMode('signup')}
-              className="text-purple-600 hover:cursor-pointer hover:text-purple-900 hover:underline">
-              SignUp</a></p>
-          ) : (
-            <p className="text-xs text-white">Already have an account? <a onClick={() => setMode('login')}
-              className="text-purple-600 hover:cursor-pointer hover:text-purple-900 hover:underline">
-              Login</a></p>
-          )}
-        </form>
-        {user &&
-          user.role === 'admin' && <p>Admin panel</p>}
+          </div>
+          <form className="flex flex-col gap-4 text-zinc-300"
+            onSubmit={handleSubmit(onSubmit)}>
+            {mode === 'signup' && (
+              <div>
+                <input
+                  className="w-full bg-zinc-700 px-3.5 py-2.5 rounded-md focus:bg-zinc-600"
+                  {...register("name", { required: "Name is required" })}
+                  type="text" placeholder="Your name" />
+                {errors.name && (
+                  <p className="text-sm text-red-500 mt-1">{errors.name.message}</p>
+                )}
+              </div>
+            )}
+            <div>
+              <input
+                className="w-full bg-zinc-700 px-3.5 py-2.5 rounded-md focus:bg-zinc-600"
+                {...register("email", { required: "Email is required" })}
+                type="email" placeholder="Email address" />
+              {errors.email && (
+                <p className="text-sm text-red-500 mt-1">{errors.email.message}</p>
+              )}
+            </div>
+            <div>
+              <input
+                className="w-full bg-zinc-700 px-3.5 py-2.5 rounded-md focus:bg-zinc-600"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 4,
+                    message: "Password should be atleast 4 characters"
+                  }
+                })}
+                type="password" placeholder="Password" />
+              {errors.password && (
+                <p className="text-sm text-red-500 mt-1">{errors.password.message}</p>
+              )}
+            </div>
+            {mode === 'login' && (
+              <div>
+                <input className="mr-1.5" type="checkbox" defaultChecked />
+                <label>Remember me</label>
+              </div>
+            )}
+            <button
+              className="bg-zinc-300 text-zinc-700 py-2 rounded-md
+                hover:cursor-pointer hover:bg-zinc-400 hover:text-zinc-800"
+              type="submit">
+              {mode === 'login' ? (
+                'Log In'
+              ) : (
+                'Sign Up'
+              )}
+            </button>
+          </form>
+        </div>
       </div>
-    </div >
+    </div>
   )
 }
--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,20 +1,52 @@
 // src/components/HeroSection.jsx
 
+import img from "../assets/pexels-vizi-kata-373820-33314427.jpg"
+
 export default function TestPage() {
+
 
   return (
     <div className="">
-      <div className="flex gap-4">
-        <div className="w-64 h-94 bg-dark border border-zinc-800 rounded-2xl">
-          <h1 className="py-2 px-4 m-2 hover:bg-zinc-900 rounded-2xs">Hello</h1>
-        </div>
-        <div className="flex flex-col gap-4">
-          <p className="text-lime-300 bg-lime-950 px-4 py-2 w-fit rounded-xl">234%</p>
-          <p className="text-pink-400 bg-red-950 px-4 py-2 w-fit rounded-xl">234%</p>
-          <button type="" className="border border-zinc-700 hover:border-zinc-600 hover:bg-zinc-800 bg-zinc-900 py-1 rounded-md text-white">Click</button>
-          <button type="" className="border border-zinc-400 hover:border-zinc-300 hover:bg-zinc-500 bg-zinc-600 rounded-md py-1 text-white">Click</button>
+      <div className="h-screen">
+        <div className="grid grid-cols-2 overflow-hidden h-full">
+          <img className="object-cover bg-center bg-cover" src={img} alt="" />
+          <div className="bg-zinc-800 px-28 flex flex-col justify-center h-screen">
+            <h1 className="absolute top-4 left-4 text-2xl font-extrabold">Trek wiki</h1>
+            <div className="mb-8">
+              <h1 className="text-6xl mb-2">Login to your account</h1>
+              <p className="text-zinc-400">Already have an account ? <a className="text-blue-500 hover:underline" href="">Sign Up</a></p>
+
+            </div>
+            <form className="flex flex-col gap-6 text-zinc-300">
+              <input
+                className="w-full bg-zinc-700 px-3.5 py-2.5 rounded-md"
+                type="email" placeholder="Email address" />
+              <input
+                className="w-full bg-zinc-700 px-3.5 py-2.5 rounded-md"
+                type="password" placeholder="Password" />
+              <div>
+                <input className="mr-1.5" type="checkbox" />
+                <label>Remember me</label>
+              </div>
+              <button
+                className="bg-zinc-500 py-2 rounded-md
+                hover:cursor-pointer hover:bg-zinc-600"
+                type="submit">Log In</button>
+            </form>
+          </div>
         </div>
       </div>
+      {/* <div className="flex gap-4"> */}
+      {/*   <div className="w-64 h-94 bg-dark border border-zinc-800 rounded-2xl"> */}
+      {/*     <h1 className="py-2 px-4 m-2 hover:bg-zinc-900 rounded-2xs">Hello</h1> */}
+      {/*   </div> */}
+      {/*   <div className="flex flex-col gap-4"> */}
+      {/*     <p className="text-lime-300 bg-lime-950 px-4 py-2 w-fit rounded-xl">234%</p> */}
+      {/*     <p className="text-pink-400 bg-red-950 px-4 py-2 w-fit rounded-xl">234%</p> */}
+      {/*     <button type="" className="border border-zinc-700 hover:border-zinc-600 hover:bg-zinc-800 bg-zinc-900 py-1 rounded-md text-white">Click</button> */}
+      {/*     <button type="" className="border border-zinc-400 hover:border-zinc-300 hover:bg-zinc-500 bg-zinc-600 rounded-md py-1 text-white">Click</button> */}
+      {/*   </div> */}
+      {/* </div> */}
     </div>
   );
 }
