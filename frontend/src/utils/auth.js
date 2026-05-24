af7534dd07bf02f2c5e44be299350c43fd7d6206
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-05-24 18:55:12 +0530

    committed

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -2,7 +2,6 @@ import { jwtDecode } from "jwt-decode"
 
 export function checkUser() {
   const token = localStorage.getItem("token")
-  console.log(token)
   if (!token) return null
   try {
     return jwtDecode(token)
